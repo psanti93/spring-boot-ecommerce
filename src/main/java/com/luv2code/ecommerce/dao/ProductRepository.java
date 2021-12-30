db7a1fc3897cbeb5c9600e5b55eef48b1b2c2e46
Paul Santiago <paulsantiago282@gmail.com>
2021-12-29 21:57:12 -0500

    Add cross origin for products and product category repositiories, make a note in application.properties of how to connect to db, create jpa repositories for  ecommerce site, and configure eccomerce site to not do puts,posts,and deletes

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -2,7 +2,11 @@ package com.luv2code.ecommerce.dao;
 
 import com.luv2code.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+//Repository maps our entities to the database
+//accept call from web service
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
