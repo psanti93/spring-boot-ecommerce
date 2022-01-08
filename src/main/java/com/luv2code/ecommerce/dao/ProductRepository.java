96b8998f3b23715833160f18be7d476318b393ad
Paul Santiago <paulsantiago282@gmail.com>
2022-01-08 15:29:38 -0500

    Add query method in repository to search by category id

--- src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -1,12 +1,17 @@
 package com.luv2code.ecommerce.dao;
 
 import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //Repository maps our entities to the database
 //accept call from web service
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 
 }
